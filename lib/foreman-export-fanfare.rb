906feb8ae157a2b9a6f57d58f20b9e75493bf3f9
Fletcher Nichol <fnichol@nichol.ca>
2012-03-06 19:20:07 -0700

    Initial import from `bundle gem foreman-export-fanfare`.

---
+++ lib/foreman-export-fanfare.rb
@@ -0,0 +1,9 @@
+require "foreman-export-fanfare/version"
+
+module Foreman
+  module Export
+    module Fanfare
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/foreman-export-fanfare/version.rb
@@ -0,0 +1,7 @@
+module Foreman
+  module Export
+    module Fanfare
+      VERSION = "0.0.1"
+    end
+  end
+end
