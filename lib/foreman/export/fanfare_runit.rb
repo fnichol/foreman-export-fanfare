60d0600e9df36df68d49f9356797cc6d52a2080f
Fletcher Nichol <fnichol@nichol.ca>
2012-03-06 21:32:30 -0700

    Initial specs to build off.

--- lib/foreman/export/fanfare_runit.rb
+++ lib/foreman/export/fanfare_runit.rb
@@ -1,8 +1,16 @@
+require 'pathname'
 require 'foreman/export'
 
 module Foreman
   module Export
     class FanfareRunit < Base
+      def export
+        error("Must specify a location") unless location
+
+        engine.procfile.entries.each do |process|
+          Service.new("#{app}-#{process.name}-1", process.command)
+        end
+      end
     end
   end
 end
--- spec/foreman/export/fanfare_runit_spec.rb
+++ spec/foreman/export/fanfare_runit_spec.rb
@@ -1,5 +1,45 @@
 require 'minitest/autorun'
+require 'mocha'
+require 'foreman/engine'
 require 'foreman/export/fanfare_runit'
 
+class Foreman::Export::FanfareRunit::Service ; end
+
 describe Foreman::Export::FanfareRunit do
+  let(:app_name)      { 'fooapp' }
+  let(:location_path) { Pathname.new('/export/home/deploy/service') }
+  let(:engine)        { Foreman::Engine.new('/Procfile') }
+  let(:procfile)      { "stub: command" }
+  let(:service)       { mock('Service') }
+
+  before do
+    File.stubs(:read => procfile)
+  end
+
+  describe "#export" do
+    subject do
+      Foreman::Export::FanfareRunit.new(
+        location_path.to_s, engine, :app => app_name)
+    end
+
+    it "raises an exception if no location is given" do
+      subject   = Foreman::Export::FanfareRunit.new(nil, engine)
+
+      proc { subject.export }.must_raise Foreman::Export::Exception
+    end
+
+    describe "for a single process type" do
+      let(:service_type)  { "clock" }
+      let(:command)       { "bundle exec clock.rb" }
+      let(:procfile)      { "#{service_type}: #{command}" }
+      let(:service_name)  { "#{app_name}-#{service_type}-1" }
+
+      it "exports the process with its options" do
+        Foreman::Export::FanfareRunit::Service.expects(:new).
+          with(service_name, command).returns(service)
+
+        subject.export
+      end
+    end
+  end
 end
