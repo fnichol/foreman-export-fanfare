4e975c3d60a3782afd7fce495a882cd604497782
Fletcher Nichol <fnichol@nichol.ca>
2012-03-07 22:00:57 -0700

    Support multiple process types and concurrency.

--- spec/foreman/export/fanfare_runit_spec.rb
+++ spec/foreman/export/fanfare_runit_spec.rb
@@ -17,11 +17,6 @@ describe Foreman::Export::FanfareRunit do
   end
 
   describe "#export" do
-    subject do
-      Foreman::Export::FanfareRunit.new(
-        service_root_path.to_s, engine, :app => app_name)
-    end
-
     it "raises an exception if no location is given" do
       subject   = Foreman::Export::FanfareRunit.new(nil, engine)
 
@@ -29,6 +24,11 @@ describe Foreman::Export::FanfareRunit do
     end
 
     describe "for a single process type" do
+      subject do
+        Foreman::Export::FanfareRunit.new(
+          service_root_path.to_s, engine, :app => app_name)
+      end
+
       let(:service_type)  { "clock" }
       let(:command)       { "bundle exec clock.rb" }
       let(:procfile)      { "#{service_type}: #{command}" }
@@ -36,18 +36,92 @@ describe Foreman::Export::FanfareRunit do
       let(:port)          { 5555 }
       let(:env)           do { 'SOME' => 'SETTINGS' } end
 
-      it "exports the process with its options" do
+      before do
         engine.stubs(:port_for).with(kind_of(Foreman::ProcfileEntry), 1, nil).
           returns(port)
         engine.stubs(:environment).returns(env)
 
+        Foreman::Export::FanfareRunit::Service.stubs(:new).
+          with(service_name, command, service_root_path,
+               { 'PORT' => port }.merge(env)).
+          returns(service)
+
+        service.stubs(:create!)
+        service.stubs(:activate!)
+      end
+
+      it "initializes a Service for the process with its options" do
         Foreman::Export::FanfareRunit::Service.expects(:new).
           with(service_name, command, service_root_path,
                { 'PORT' => port }.merge(env)).
           returns(service)
 
+        subject.export
+      end
+
+      it "creates the Service" do
         service.expects(:create!).once
+
+        subject.export
+      end
+
+      it "activates the Service" do
         service.expects(:activate!).once
+
+        subject.export
+      end
+    end
+
+    describe "for multiple process types with concurrency options" do
+      subject do
+        Foreman::Export::FanfareRunit.new(
+          service_root_path.to_s, engine,
+          :app => app_name, :concurrency => 'web=1,worker=2')
+      end
+
+      let(:procfile) do
+        <<-PROCFILE.gsub(/^ {10}/, '')
+          web:    bundle exec servars
+          worker: bundle exec wurkarz
+        PROCFILE
+      end
+
+      before do
+        Foreman::Export::FanfareRunit::Service.stubs(:new).returns(service)
+        service.stubs(:create!)
+        service.stubs(:activate!)
+      end
+
+      it "initializes a Service for the web process" do
+        Foreman::Export::FanfareRunit::Service.expects(:new).
+          with("fooapp-web-1", "bundle exec servars",
+               service_root_path, kind_of(Hash)).
+          returns(service)
+
+        subject.export
+      end
+
+      it "initializes 2 Services for the worker processes" do
+        Foreman::Export::FanfareRunit::Service.expects(:new).
+          with("fooapp-worker-1", "bundle exec wurkarz",
+               service_root_path, kind_of(Hash)).
+          returns(service)
+        Foreman::Export::FanfareRunit::Service.expects(:new).
+          with("fooapp-worker-2", "bundle exec wurkarz",
+               service_root_path, kind_of(Hash)).
+          returns(service)
+
+        subject.export
+      end
+
+      it "creates the Services" do
+        service.expects(:create!).times(3)
+
+        subject.export
+      end
+
+      it "activates the Services" do
+        service.expects(:activate!).times(3)
 
         subject.export
       end
