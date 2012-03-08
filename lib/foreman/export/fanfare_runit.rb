266d4dd82e2241b5d151a2d5104a25d1844d8ae3
Fletcher Nichol <fnichol@nichol.ca>
2012-03-07 21:28:12 -0700

    Single process.

--- lib/foreman/export/fanfare_runit.rb
+++ lib/foreman/export/fanfare_runit.rb
@@ -7,8 +7,18 @@ module Foreman
       def export
         error("Must specify a location") unless location
 
+        @location = Pathname.new(@location)
+
         engine.procfile.entries.each do |process|
-          Service.new("#{app}-#{process.name}-1", process.command)
+          1.upto(self.concurrency[process.name]) do |num|
+            service_name  = "#{app}-#{process.name}-#{num}"
+            port          = engine.port_for(process, num, self.port)
+            env           = { 'PORT' => port }.merge(engine.environment)
+
+            service = Service.new(service_name, process.command, location, env)
+            service.create!
+            service.activate!
+          end
         end
       end
     end
--- spec/foreman/export/fanfare_runit_spec.rb
+++ spec/foreman/export/fanfare_runit_spec.rb
@@ -6,11 +6,11 @@ require 'foreman/export/fanfare_runit'
 class Foreman::Export::FanfareRunit::Service ; end
 
 describe Foreman::Export::FanfareRunit do
-  let(:app_name)      { 'fooapp' }
-  let(:location_path) { Pathname.new('/export/home/deploy/service') }
-  let(:engine)        { Foreman::Engine.new('/Procfile') }
-  let(:procfile)      { "stub: command" }
-  let(:service)       { mock('Service') }
+  let(:app_name)          { 'fooapp' }
+  let(:service_root_path) { Pathname.new('/export/home/deploy/service') }
+  let(:engine)            { Foreman::Engine.new('/Procfile') }
+  let(:procfile)          { "stub: command" }
+  let(:service)           { mock('Service') }
 
   before do
     File.stubs(:read => procfile)
@@ -19,7 +19,7 @@ describe Foreman::Export::FanfareRunit do
   describe "#export" do
     subject do
       Foreman::Export::FanfareRunit.new(
-        location_path.to_s, engine, :app => app_name)
+        service_root_path.to_s, engine, :app => app_name)
     end
 
     it "raises an exception if no location is given" do
@@ -33,10 +33,21 @@ describe Foreman::Export::FanfareRunit do
       let(:command)       { "bundle exec clock.rb" }
       let(:procfile)      { "#{service_type}: #{command}" }
       let(:service_name)  { "#{app_name}-#{service_type}-1" }
+      let(:port)          { 5555 }
+      let(:env)           do { 'SOME' => 'SETTINGS' } end
 
       it "exports the process with its options" do
+        engine.stubs(:port_for).with(kind_of(Foreman::ProcfileEntry), 1, nil).
+          returns(port)
+        engine.stubs(:environment).returns(env)
+
         Foreman::Export::FanfareRunit::Service.expects(:new).
-          with(service_name, command).returns(service)
+          with(service_name, command, service_root_path,
+               { 'PORT' => port }.merge(env)).
+          returns(service)
+
+        service.expects(:create!).once
+        service.expects(:activate!).once
 
         subject.export
       end
